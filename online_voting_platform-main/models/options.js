57905ae393459ed5677636a42c4137a5c128c892
sanjanpallerla <113688942+sanjanpallerla@users.noreply.github.com>
2023-01-09 23:34:01 +0530

    level11

---
+++ online_voting_platform-main/models/options.js
@@ -0,0 +1,77 @@
+"use strict";
+const { Model, Op } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Options extends Model {
+    
+    static createOption({ option, questionId }) {
+      let createOption = this.create({
+        option,
+        questionId,
+      });
+      return createOption;
+    }
+
+    static retriveOptions(questionId) {
+      let retriveOptions = this.findAll({
+        where: {
+          questionId,
+        },
+        order: [["id", "ASC"]],
+      });
+      return retriveOptions;
+    }
+
+    static retriveOption(id) {
+      let retriveOption = this.findOne({
+        where: {
+          id,
+        },
+      });
+      return retriveOption;
+    }
+
+    static deleteOption(id) {
+      let deleteOption = this.destroy({
+        where: {
+          id,
+        },
+      });
+      return deleteOption;
+    }
+
+    static editOption({ option, id }) {
+      let editOption = this.update(
+        {
+          option,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      return editOption;
+    }
+
+    static associate(models) {
+      // define association here
+      Options.belongsTo(models.questions, {
+        foreignKey: "questionId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Options.init(
+    {
+      option: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Options",
+    }
+  );
+  return Options;
+};
